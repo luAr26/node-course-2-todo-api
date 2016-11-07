e11594be6dd3ad2ad6976fb3f25402b119fb58a1
Sirb Raul Cosmin <raul_cosmin_26@yahoo.com>
2016-11-07 20:43:26 +0200

    Add tests for DELETE /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -92,3 +92,38 @@ describe('GET /todos/:id', () => {
       .end(done);
   });
 });
+describe('DELETE /todos/:id', () => {
+  it('should remove a todo', (done) => {
+    var hexId = todos[1]._id.toHexString();
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo._id).toBe(hexId);
+      })
+      .end((error, res) => {
+        if (error) {
+          return done(error);
+        }
+        Todo.findById(hexId).then((todo) => {
+          expect(todo).toNotExist();
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+
+  it('should return 404 if todo not found', (done) => {
+    request(app)
+      .delete(`todos/${new ObjectID().toHexString()}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 404 if object id is invalid', (done) => {
+    request(app)
+      .delete(`todos/${new ObjectID()}1`)
+      .expect(404)
+      .end(done);
+  });
+
+});
