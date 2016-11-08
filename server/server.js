43d16e377136a6baaa1ee0096c7801d70cd6fa52
Sirb Raul Cosmin <raul_cosmin_26@yahoo.com>
2016-11-08 19:24:17 +0200

    Add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -101,6 +101,19 @@ app.patch('/todos/:id', (req, res) => {
   });
 });
 
+// CREATE NEW USER
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e.errmsg);
+  });
+});
+
 app.listen(port, () => {
   console.log(`Started up at port ${port}...`);
 });
